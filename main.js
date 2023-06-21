640fa646786104bbb66435924f8d3b0bad0a21a9
HoangAncient <99626756+HoangAncient@users.noreply.github.com>
2023-06-21 22:54:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var fullName = 'Dang Ngoc Son'
+var age = 26
+
+
+/*Noi
+dung o day
+la comment
+
+
+*/
+
+/**
+ * Khai bao bien
+ * Goi ham thong bao
+ */
+
+/*
+    Khai bao bien
+    Goi ham thong bao
+*/
